3738f58620c99d9571794200f377207172c68494
Stefan <slotties@gmail.com>
2015-11-04 20:02:09 +0100

    pull all listings and print them onto System.out

--- import/src/main/java/gw2trades/importer/Main.java
+++ import/src/main/java/gw2trades/importer/Main.java
@@ -3,8 +3,9 @@ package gw2trades.importer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gw2trades.importer.dao.TradingPost;
 import gw2trades.importer.http.ApiClient;
+import gw2trades.importer.model.CommerceListings;
 
-import java.util.Collection;
+import java.util.List;
 
 /**
  * TODO
@@ -19,8 +20,22 @@ public class Main {
         tradingPost.setApiClient(apiClient);
         tradingPost.setObjectMapper(objectMapper);
 
-        Collection<Integer> itemIds = tradingPost.listItemIds();
-        System.out.println(itemIds);
-        // TODO: pull item pricings and listings ...
+        List<Integer> itemIds = tradingPost.listItemIds();
+        int batchSize = 50;
+        int batches = (int) Math.ceil((float) itemIds.size() / (float) batchSize);
+        for (int i = 0; i < batches; i++) {
+            System.out.printf("Pulling batch %d of %d (total %d items)...\n",
+                    i, batches, itemIds.size());
+
+            // TODO: implement parallel processing to improve speed
+            List<Integer> itemIdBatch = itemIds.subList(i * batchSize, Math.min(itemIds.size() - 1, (i + 1) * batchSize));
+            List<CommerceListings> listings = tradingPost.listings(itemIdBatch);
+            // TODO: import listings somewhere
+            for (CommerceListings listing : listings) {
+                System.out.printf("%d: %d buyers, %d sellers\n",
+                        listing.getItemId(),
+                        listing.getBuys().size(), listing.getSells().size());
+            }
+        }
     }
 }
