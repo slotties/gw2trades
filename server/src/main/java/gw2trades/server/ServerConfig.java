04f7098c1646168d960016baca9a7070de204991
Stefan <slotties@gmail.com>
2015-11-16 18:57:32 +0100

    configure the repository via spring

--- server/src/main/java/gw2trades/server/ServerConfig.java
+++ server/src/main/java/gw2trades/server/ServerConfig.java
@@ -1,8 +1,17 @@
 package gw2trades.server;
 
+import gw2trades.repository.api.ItemRepository;
+import gw2trades.repository.filesystem.FilesystemItemRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.File;
 
 /**
  * @author Stefan Lotties (slotties@gmail.com)
@@ -10,4 +19,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @ConfigurationProperties
 public class ServerConfig extends WebMvcConfigurerAdapter {
+    private static final Logger LOGGER = LogManager.getLogger(ServerConfig.class);
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public ItemRepository itemRepository() {
+        File directory = new File(this.environment.getProperty("repo.fs.dir"));
+        LOGGER.info("Using {} as filesystem repository directory.", directory.getAbsolutePath());
+
+        return new FilesystemItemRepository(directory);
+    }
 }
