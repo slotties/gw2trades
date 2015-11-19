d94fc86d5b2e06bd751144f3e4a0ae8bd4487c6d
Stefan <slotties@gmail.com>
2015-11-19 20:20:25 +0100

    added average lines

--- server/src/main/resources/static/js/history.js
+++ server/src/main/resources/static/js/history.js
@@ -5,10 +5,15 @@
         bottom: 30,
         left: 50
     },
-    width = 960 - margin.left - margin.right,
+    width = d3.select("#priceHistory")[0][0].clientWidth - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom,
     data = gw2historyData,
-    maxPrice = d3.max(data, function(d) { return Math.max(d.buyStatistics.maxPrice, d.sellStatistics.minPrice); });
+    maxPrice = d3.max(data, function(d) {
+        return Math.max(
+            Math.max(d.buyStatistics.maxPrice, d.sellStatistics.minPrice),
+            Math.max(d.buyStatistics.average, d.sellStatistics.average)
+        );
+    });
 
     var x = d3.time.scale()
         .range([0, width])
@@ -30,10 +35,16 @@
     var sellers = d3.svg.line()
         .x(function(d) { return x(d.timestamp); })
         .y(function(d) { return y(d.sellStatistics.minPrice); });
+    var sellersAvg = d3.svg.line()
+        .x(function(d) { return x(d.timestamp); })
+        .y(function(d) { return y(d.sellStatistics.average); });
 
     var buyers  = d3.svg.line()
         .x(function(d) { return x(d.timestamp); })
         .y(function(d) { return y(d.buyStatistics.maxPrice); });
+    var buyersAvg  = d3.svg.line()
+        .x(function(d) { return x(d.timestamp); })
+        .y(function(d) { return y(d.buyStatistics.average); });
 
     var svg = d3.select("#priceHistory").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -54,11 +65,18 @@
         .datum(data)
         .attr("class", "gw2-history-sellers")
         .attr("d", sellers);
-
     svg.append("path")
         .datum(data)
         .attr("class", "gw2-history-buyers")
         .attr("d", buyers);
+    svg.append("path")
+        .datum(data)
+        .attr("class", "gw2-history-sellers-avg")
+        .attr("d", sellersAvg);
+    svg.append("path")
+        .datum(data)
+        .attr("class", "gw2-history-buyers-avg")
+        .attr("d", buyersAvg);
 
     svg.append("rect")
         .attr("class", "gw2-charts-overlay")
