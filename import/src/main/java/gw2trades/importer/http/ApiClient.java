38e03c2a982f2d75bb1ccb0c9d3bf6bcfc02a5d4
Stefan <slotties@gmail.com>
2015-11-22 19:46:30 +0100

    added logger and set it for debug for the first few productive days

--- import/src/main/java/gw2trades/importer/http/ApiClient.java
+++ import/src/main/java/gw2trades/importer/http/ApiClient.java
@@ -1,5 +1,8 @@
 package gw2trades.importer.http;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.*;
 import java.net.URL;
 import java.util.stream.Collectors;
@@ -9,6 +12,9 @@ import java.util.stream.Collectors;
  * @author Stefan Lotties (slotties@gmail.com)
  */
 public class ApiClient {
+    private static final Logger LOGGER = LogManager.getLogger(ApiClient.class);
+
+
     /**
      * Returns the response body of a given URL.
      * @param urlStr the URL to call
@@ -21,8 +27,14 @@ public class ApiClient {
         URL url = new URL(urlStr);
         String str;
 
+        long t0 = System.currentTimeMillis();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"), 8 * 1024)) {
             str = reader.lines().collect(Collectors.joining());
+        }
+
+        if (LOGGER.isDebugEnabled()) {
+            long t1 = System.currentTimeMillis();
+            LOGGER.debug("Pulling from {} took {} ms.", urlStr, t1 - t0);
         }
 
         return str.toString();
