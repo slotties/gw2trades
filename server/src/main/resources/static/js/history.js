4cd4fda77073f9b2def45bb8fb84ca526c5c910e
Stefan <slotties@gmail.com>
2015-11-23 20:44:42 +0100

    quickly implemented supply/demand graphs

--- server/src/main/resources/static/js/history.js
+++ server/src/main/resources/static/js/history.js
@@ -98,7 +98,96 @@
     // TODO: load data per xhr
     // TODO: legend
     // TODO: line namings
-    // TODO: better x-axis (per day, etc.)
+    // TODO: "mini-map"/long term timeline
+    // TODO: some orientational lines
+})();
+
+(function() {
+    var margin = {
+        top: 20,
+        right: 50,
+        bottom: 30,
+        left: 50
+    },
+    width = d3.select("#priceHistory")[0][0].clientWidth - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom,
+    data = gw2historyData,
+    totalAmount = d3.max(data, function(d) {
+        return Math.max(
+                d.buyStatistics.totalAmount,
+                d.sellStatistics.totalAmount
+        );
+    });
+
+    var x = d3.time.scale()
+        .range([0, width])
+        .domain([
+            // TODO: scala depending on the selected time frame
+            new Date(data[data.length - 1].timestamp - (7 * 24 * 60 * 60 * 1000)),
+            new Date(data[data.length - 1].timestamp)
+        ]);
+
+    var y = d3.scale.linear()
+        .range([height, 0])
+        .domain([ 0, totalAmount * 1.1 ]);
+
+    var xTickFormat = d3.time.format.multi([
+        ["%I:%M", function(d) { return d.getMinutes(); }],
+        ["%H:%M", function(d) { return d.getHours(); }],
+        ["%e %b", function(d) { return true; }]
+    ]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .tickFormat(xTickFormat)
+        .ticks(d3.time.hours, 8);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var sellers = d3.svg.line()
+        .x(function(d) { return x(d.timestamp); })
+        .y(function(d) { return y(d.sellStatistics.totalAmount); });
+
+    var buyers  = d3.svg.line()
+        .x(function(d) { return x(d.timestamp); })
+        .y(function(d) { return y(d.buyStatistics.totalAmount); });
+
+    var svg = d3.select("#supplyHistory").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("g")
+        .attr("class", "gw2-charts-x gw2-charts-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "gw2-charts-y gw2-charts-axis")
+        .call(yAxis);
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "gw2-history-sellers")
+        .attr("d", sellers);
+    svg.append("path")
+        .datum(data)
+        .attr("class", "gw2-history-buyers")
+        .attr("d", buyers);
+
+    svg.append("rect")
+        .attr("class", "gw2-charts-overlay")
+        .attr("width", width)
+        .attr("height", height);
+
+    // TODO: mouse-over
+    // TODO: load data per xhr
+    // TODO: legend
+    // TODO: line namings
     // TODO: "mini-map"/long term timeline
     // TODO: some orientational lines
 })();
