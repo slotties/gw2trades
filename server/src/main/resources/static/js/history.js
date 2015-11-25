b85089ad8a462e6350243e6b33102355a967eb25
Stefan <slotties@gmail.com>
2015-11-25 21:41:37 +0100

    migrated supply/demand chart as well

--- server/src/main/resources/static/js/history.js
+++ server/src/main/resources/static/js/history.js
@@ -1,36 +1,9 @@
 // TODO: load data per xhr
+// TODO: bring back labels
+// TODO: fix high density x axis
+// TODO: bring back tooltips
 
 (function() {
-    var line = function(conf) {
-        this.line = d3.svg.line().x(conf.xFn).y(conf.yFn);
-        this.path = conf.svg.append("path")
-            .datum(conf.data)
-            .attr("class", conf.cls)
-            .attr("d", this.line);
-
-        this.focusCircle = conf.svg.append("g")
-            .attr("class", conf.focusCls)
-            .style("display", "none");
-
-        this.focusCircle.append("circle").attr("r", 3);
-
-        conf.svg.append("text")
-        		.attr("transform", "translate(" + conf.labelX + "," + conf.labelY + ")")
-        		.attr("dy", ".35em")
-        		.attr('class', conf.cls)
-        		.attr("text-anchor", "start")
-        		.text(conf.label);
-    };
-    line.prototype.hideHighlight = function() {
-        this.focusCircle.style("display", "none");
-    };
-    line.prototype.showHighlight = function() {
-        this.focusCircle.style("display", null);
-    };
-    line.prototype.highlight = function(x, y) {
-        this.focusCircle.attr("transform", "translate(" + x + "," + y + ")");
-    };
-
     var tooltip = function(element) {
         this.element = element;
     };
@@ -50,38 +23,7 @@
         this.element.style.display = 'none';
     };
 
-    var createTimeAxis = function(width, fromTimestamp, toTimestamp) {
-        var scale = d3.time.scale()
-            .range([0, width])
-            .domain([ fromTimestamp, toTimestamp ]);
-
-        var tickFormat = d3.time.format.multi([
-            ["%H:%M", function(d) { return d.getHours(); }],
-            ["%e %b", function(d) { return true; }]
-        ]);
-
-        var axis = d3.svg.axis()
-            .scale(scale)
-            .orient("bottom")
-            .tickFormat(tickFormat)
-            .ticks(d3.time.hours, 8);
-
-        return {
-            scale: scale,
-            axis: axis
-        };
-    };
-
     var bisectDate = d3.bisector(function(d) { return d.timestamp; }).left;
-    var selectDataPoint = function(data, eventHandler, scale) {
-        var x0 = scale.invert(d3.mouse(eventHandler)[0]),
-            i = bisectDate(data, x0, 1),
-            d0 = data[i - 1],
-            d1 = data[i],
-            d = x0 - d0.timestamp > d1.timestamp - x0 ? d1 : d0;
-
-        return d;
-    };
 
     var renderCoins = function(coins) {
         coins = Math.floor(coins);
@@ -157,7 +99,11 @@
                 if (!self.data || self.data.length < 2) {
                     return;
                 }
-                var d = gw2charts.selectDataPoint(self.data, this, self.x),
+                var x0 = self.x.invert(d3.mouse(this)[0]),
+                    i = bisectDate(self.data, x0, 1),
+                    d0 = self.data[i - 1],
+                    d1 = self.data[i],
+                    d = x0 - d0.timestamp > d1.timestamp - x0 ? d1 : d0,
                     x = self.x(d.timestamp);
 
                 self.lines.forEach(function(line) {
@@ -213,10 +159,7 @@
     };
 
     window.gw2charts = {
-        Line: line,
         Tooltip: tooltip,
-        createTimeAxis: createTimeAxis,
-        selectDataPoint: selectDataPoint,
         Chart: chart
     };
 })();
@@ -236,6 +179,16 @@
             });
 
         return [ 0, maxPrice * 1.1 ];
+    },
+    yScaleSupplyDemandFn = function(data) {
+        var totalAmount = d3.max(data, function(d) {
+            return Math.max(
+                    d.buyStatistics.totalAmount,
+                    d.sellStatistics.totalAmount
+            );
+        });
+
+        return [ 0, totalAmount ];
     },
     createPriceHistoryChart = function() {
         var chart = new gw2charts.Chart(d3.select("#priceHistory"));
@@ -265,9 +218,27 @@
         });
 
         return chart;
+    },
+    createSupplyDemandChart = function() {
+        var chart = new gw2charts.Chart(d3.select("#supplyDemand"));
+        chart.add({
+            yFn: function(d, y) { return y(d.sellStatistics.totalAmount); },
+            label: 'Sellers',
+            cls: 'gw2-history-sellers',
+            focusCls: 'gw2-history-sellers-focus'
+        });
+        chart.add({
+            yFn: function(d, y) { return y(d.buyStatistics.totalAmount); },
+            label: 'Buyers',
+            cls: 'gw2-history-buyers',
+            focusCls: 'gw2-history-buyers-focus'
+        });
+
+        return chart;
     };
 
-    var priceHistory = createPriceHistoryChart();
+    var priceHistory = createPriceHistoryChart(),
+        supplyDemand = createSupplyDemandChart();
 
     var onTimeselectorClick = function(btn) {
         var offset = btn.getAttribute('data-offset').split(/([0-9]*)/g),
@@ -295,6 +266,9 @@
         d3.json('/api/history/' + gw2scope.itemId + '?from=' + fromStr + '&to=' + toStr, function(err, data) {
             var yScale = yScalePriceHistoryFn(data);
             priceHistory.update(data, [ from, to ], yScale);
+
+            yScale = yScaleSupplyDemandFn(data);
+            supplyDemand.update(data, [ from, to ], yScale);
         });
     };
 
@@ -304,94 +278,4 @@
             onTimeselectorClick(this);
         });
     };
-})();
-
-(function() {
-    var margin = {
-        top: 20,
-        right: 150,
-        bottom: 30,
-        left: 50
-    },
-    width = d3.select("#supplyHistory")[0][0].clientWidth - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom,
-    data = gw2historyData,
-    totalAmount = d3.max(data, function(d) {
-        return Math.max(
-                d.buyStatistics.totalAmount,
-                d.sellStatistics.totalAmount
-        );
-    }),
-    bisectDate = d3.bisector(function(d) { return d.timestamp; }).left;
-
-    var timeAxis = gw2charts.createTimeAxis(width,
-            // TODO: scala depending on the selected time frame
-            new Date(data[data.length - 1].timestamp - (7 * 24 * 60 * 60 * 1000)),
-            new Date(data[data.length - 1].timestamp)
-        );
-    var x = timeAxis.scale;
-
-    var y = d3.scale.linear()
-        .range([height, 0])
-        .domain([ 0, totalAmount * 1.1 ]);
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var svg = d3.select("#supplyHistory").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var sellers = new gw2charts.Line({
-        svg: svg,
-        data: data,
-        xFn: function(d) { return x(d.timestamp); },
-        yFn: function(d) { return y(d.sellStatistics.totalAmount); },
-        labelX: width + 4,
-        labelY: y(data[data.length - 1].sellStatistics.totalAmount),
-        label: 'Selles',
-        cls: 'gw2-history-sellers',
-        focusCls: 'gw2-history-sellers-focus'
-    });
-    var buyers = new gw2charts.Line({
-        svg: svg,
-        data: data,
-        xFn: function(d) { return x(d.timestamp); },
-        yFn: function(d) { return y(d.buyStatistics.totalAmount); },
-        labelX: width + 4,
-        labelY: y(data[data.length - 1].buyStatistics.totalAmount),
-        label: 'Buyers',
-        cls: 'gw2-history-buyers',
-        focusCls: 'gw2-history-buyers-focus'
-    });
-
-    svg.append("g")
-        .attr("class", "gw2-charts-x gw2-charts-axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(timeAxis.axis);
-
-    svg.append("g")
-        .attr("class", "gw2-charts-y gw2-charts-axis")
-        .call(yAxis);
-
-    svg.append("rect")
-        .attr("class", "gw2-charts-overlay")
-        .attr("width", width)
-        .attr("height", height)
-        .on("mouseover", function() {
-            sellers.showHighlight();
-            buyers.showHighlight();
-        })
-        .on("mouseout", function() {
-            sellers.hideHighlight();
-            buyers.hideHighlight();
-        })
-        .on("mousemove", function() {
-            var d = gw2charts.selectDataPoint(data, this, timeAxis.scale);
-            sellers.highlight(x(d.timestamp), y(d.sellStatistics.totalAmount));
-            buyers.highlight(x(d.timestamp), y(d.buyStatistics.totalAmount));
-        });
 })();
