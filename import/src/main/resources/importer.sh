d1d7d3a93a0b74655b54e7a4290f6828123808dc
Stefan <slotties@gmail.com>
2015-11-28 13:57:24 +0100

    implemented importer.sh

--- import/src/main/resources/importer.sh
+++ import/src/main/resources/importer.sh
@@ -1,1 +1,13 @@
-echo TODO
+#!/bin/bash
+
+set CLASSPATH=
+
+for i in lib/*; do
+        CLASSPATH=$CLASSPATH:$i
+done
+
+LOG="log/importer_`date +%Y-%m-%d:%H:%M:%S`"
+
+mkdir ./log
+echo Logging to $LOG
+java -cp $CLASSPATH gw2trades.importer.Main > $LOG  2>&1
