bc930e2330640b2f4deb0cb69df967b5f8c082b5
Stefan <slotties@gmail.com>
2015-11-29 14:08:42 +0100

    configure log dir via parameter

--- import/src/main/resources/importer.sh
+++ import/src/main/resources/importer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+LOG_DIR=$1
 set CLASSPATH=
 
 for i in lib/*; do
         CLASSPATH=$CLASSPATH:$i
 done
 
-LOG="log/importer_`date +%Y-%m-%d:%H:%M:%S`"
+LOG="$LOG_DIR/importer_`date +%Y-%m-%d:%H:%M:%S`"
 
-mkdir ./log
 echo Logging to $LOG
 java -cp $CLASSPATH gw2trades.importer.Main > $LOG  2>&1
