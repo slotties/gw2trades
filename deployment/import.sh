1f41951dd292ea8e0294340381daa7b4280c905e
Stefan <slotties@gmail.com>
2015-11-30 20:04:36 +0100

    fixed script

--- deployment/import.sh
+++ deployment/import.sh
@@ -4,7 +4,7 @@ IMPORTER_SH=$1
 LOG_DIR=$2
 INDEX_DIR=$3
 
-if [ ! -f "IMPORTER_SH" ]; then
+if [ ! -f "$IMPORTER_SH" ]; then
     echo "Could not find $IMPORTER_SH"
     exit 1
 fi
@@ -12,7 +12,7 @@ if [ ! -d "$LOG_DIR" ]; then
     echo "Could not find log directory $LOG_DIR"
     exit 1
 fi
-if [ ! -d "INDEX_DIR" ]; then
+if [ ! -d "$INDEX_DIR" ]; then
     echo "Could not find index directory $INDEX_DIR"
     exit 1
 fi
