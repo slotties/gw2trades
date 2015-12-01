9614e8ff272b486b71b4f90c9749a42a3cf3419e
Stefan <slotties@gmail.com>
2015-12-01 21:08:25 +0100

    added /admin/sitemap.xml

--- server/src/main/java/gw2trades/server/frontend/AdminController.java
+++ server/src/main/java/gw2trades/server/frontend/AdminController.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author Stefan Lotties (slotties@gmail.com)
@@ -24,5 +27,18 @@ public class AdminController {
     public @ResponseBody String reopenRepository() throws IOException {
         this.itemRepository.reopen();
         return "OK";
+    }
+
+    @RequestMapping("/admin/sitemap.xml")
+    public ModelAndView createSitemapXml() throws IOException {
+        ModelAndView mav = new ModelAndView("sitemapXml");
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String now = sdf.format(new Date());
+
+        mav.addObject("stats", itemRepository.listStatistics(null, null, 0, 5000));
+        mav.addObject("now", now);
+
+        return mav;
     }
 }
