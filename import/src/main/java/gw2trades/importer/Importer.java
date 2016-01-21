4f72dc9a200acf0acb23c498cad5f444c60c580f
Stefan <slotties@gmail.com>
2016-01-21 21:33:23 +0100

    added recipe importer

--- import/src/main/java/gw2trades/importer/Importer.java
+++ import/src/main/java/gw2trades/importer/Importer.java
@@ -2,11 +2,14 @@ package gw2trades.importer;
 
 import gw2trades.importer.dao.TradingPost;
 import gw2trades.repository.api.ItemRepository;
+import gw2trades.repository.api.RecipeRepository;
 import gw2trades.repository.api.model.Item;
 import gw2trades.repository.api.model.ItemListings;
+import gw2trades.repository.api.model.Recipe;
 import gw2trades.repository.influxdb.InfluxDbConnectionManager;
 import gw2trades.repository.influxdb.InfluxDbConnectionManagerImpl;
 import gw2trades.repository.influxdb.InfluxDbRepository;
+import gw2trades.repository.lucene.LuceneRecipeRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.influxdb.InfluxDB;
@@ -50,9 +53,15 @@ public class Importer {
 
         LOGGER.info("Importing items ...");
         long t0 = System.currentTimeMillis();
-        importItems(threadCount, chunkSize, connectionManager, indexDir);
+        importItems(threadCount, chunkSize, connectionManager, indexDir + "/items");
         long t1 = System.currentTimeMillis();
-        LOGGER.info("Imported the trading post within {} ms.", t1 - t0);
+        LOGGER.info("Imported items in {} ms.", t1 - t0);
+
+        LOGGER.info("Importing recipes ...");
+        t0 = System.currentTimeMillis();
+        importRecipes(threadCount, chunkSize, indexDir + "/recipes");
+        t1 = System.currentTimeMillis();
+        LOGGER.info("Imported recipes in {} ms.", t1 - t0);
     }
 
     private void importItems(int threadCount, int chunkSize, InfluxDbConnectionManager connectionManager, String indexDir) throws Exception {
@@ -64,7 +73,7 @@ public class Importer {
         List<Integer> itemIds = tradingPost.listItemIds();
         for (int i = 0; i < itemIds.size(); i += chunkSize) {
             List<Integer> chunk = itemIds.subList(i, Math.min(itemIds.size(), i + chunkSize));
-            executorService.execute(new DataPuller(tradingPost, chunk, itemCache, allListings));
+            executorService.execute(new ItemPuller(tradingPost, chunk, itemCache, allListings));
         }
 
         executorService.shutdown();
@@ -82,7 +91,34 @@ public class Importer {
         } finally {
             repository.close();
         }
+    }
 
+    private void importRecipes(int threadCount, int chunkSize, String indexDir) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+
+        Map<Integer, Recipe> recipes = new ConcurrentHashMap<>();
+
+        List<Integer> ids = tradingPost.listRecipeIds();
+        for (int i = 0; i < ids.size(); i += chunkSize) {
+            List<Integer> chunk = ids.subList(i, Math.min(ids.size(), i + chunkSize));
+            executorService.execute(new RecipePuller(tradingPost, chunk, recipes));
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.MINUTES);
+
+        if (recipes.isEmpty()) {
+            LOGGER.error("The gw2 API did not return any recipes. It's broken again. Won't import anything.");
+            System.exit(1);
+        }
+
+        RecipeRepository repository = new LuceneRecipeRepository(indexDir, false);
+        try {
+            LOGGER.info("Writing everything into repository ...");
+            repository.store(recipes.values());
+        } finally {
+            repository.close();
+        }
     }
 
     private void setupDatabase(InfluxDbConnectionManager influxDbConnectionManager) {
@@ -94,13 +130,13 @@ public class Importer {
         }
     }
 
-    static final class DataPuller implements Runnable {
+    static final class ItemPuller implements Runnable {
         private final TradingPost tradingPost;
         private final List<Integer> itemIdsChunk;
         private final Map<Integer, Item> itemCache;
         private final Map<Integer, ItemListings> allListings;
 
-        public DataPuller(TradingPost tradingPost, List<Integer> itemIdsChunk, Map<Integer, Item> itemCache, Map<Integer, ItemListings> allListings) {
+        public ItemPuller(TradingPost tradingPost, List<Integer> itemIdsChunk, Map<Integer, Item> itemCache, Map<Integer, ItemListings> allListings) {
             this.tradingPost = tradingPost;
             this.itemIdsChunk = itemIdsChunk;
             this.itemCache = itemCache;
@@ -130,6 +166,32 @@ public class Importer {
                 }
             } catch (IOException e) {
                 LOGGER.error("Could not import item ids {}", itemIdsChunk, e);
+            }
+        }
+    }
+
+    static final class RecipePuller implements Runnable {
+        private final TradingPost tradingPost;
+        private final List<Integer> ids;
+        private final Map<Integer, Recipe> allRecipes;
+
+        public RecipePuller(TradingPost tradingPost, List<Integer> ids, Map<Integer, Recipe> allRecipes) {
+            this.tradingPost = tradingPost;
+            this.ids = ids;
+            this.allRecipes = allRecipes;
+        }
+
+        @Override
+        public void run() {
+            try {
+                List<Recipe> recipes = tradingPost.listRecipes(ids);
+
+                // Register all listings.
+                for (Recipe recipe : recipes) {
+                    allRecipes.put(recipe.getId(), recipe);
+                }
+            } catch (IOException e) {
+                LOGGER.error("Could not import recipes {}", ids, e);
             }
         }
     }
