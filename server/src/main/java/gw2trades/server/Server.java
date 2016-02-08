470c9d8ed825a35ae5db0392d2b7cb6f3d3e9412
Stefan <slotties@gmail.com>
2016-02-08 15:44:07 +0100

    integrated /admin/reopenRepositories

--- server/src/main/java/gw2trades/server/Server.java
+++ server/src/main/java/gw2trades/server/Server.java
@@ -1,24 +1,66 @@
 package gw2trades.server;
 
+import gw2trades.repository.api.ItemRepository;
+import gw2trades.repository.api.RecipeRepository;
+import gw2trades.repository.influxdb.InfluxDbConnectionManagerImpl;
+import gw2trades.repository.influxdb.InfluxDbRepository;
+import gw2trades.repository.lucene.LuceneRecipeRepository;
+import gw2trades.server.frontend.ReopenRepositoryHandler;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Stefan Lotties (slotties@gmail.com)
  */
 public class Server extends AbstractVerticle {
+    private static final Logger LOGGER = LogManager.getLogger(ServerConfig.class);
+
+    private ItemRepository itemRepository;
+    private RecipeRepository recipeRepository;
+
     @Override
     public void start() throws Exception {
+        openRepositories();
+        startWebServer();
+    }
+
+    private void openRepositories() throws IOException {
+        InfluxDbConnectionManagerImpl connectionManager = new InfluxDbConnectionManagerImpl(
+                config().getString("influx.url"),
+                config().getString("influx.user"),
+                config().getString("influx.pass")
+        );
+
+        String indexDir = config().getString("index.dir.items");
+        LOGGER.info("Using {} as item index directory.", indexDir);
+
+        this.itemRepository = new InfluxDbRepository(connectionManager, indexDir, true);
+
+        indexDir = config().getString("index.dir.recipes");
+        LOGGER.info("Using {} as recipe index directory.", indexDir);
+
+        this.recipeRepository = new LuceneRecipeRepository(indexDir, true);
+    }
+
+    private void startWebServer() {
         HttpServer server = vertx.createHttpServer();
 
         Router router = Router.router(vertx);
-        router.routeWithRegex("/static/.*").handler(StaticHandler.create(new File(".").getAbsolutePath()));
 
-        // TODO: read port from config
-        server.requestHandler(router::accept).listen(8080);
+        router.routeWithRegex("/static/.*").handler(
+                StaticHandler
+                        .create(new File(".").getAbsolutePath())
+                        .setCachingEnabled(config().getBoolean("resources.disableCaching", false))
+        );
+        router.routeWithRegex("/admin/reopenRepository").handler(new ReopenRepositoryHandler(itemRepository, recipeRepository));
+
+        server.requestHandler(router::accept).listen(config().getInteger("http.port"));
     }
 }
--- server/src/main/java/gw2trades/server/ServerConfig.java
+++ server/src/main/java/gw2trades/server/ServerConfig.java
@@ -14,82 +14,10 @@ public class ServerConfig { /*extends WebMvcConfigurerAdapter {
     private VelocityProperties properties;
 
     @Bean
-    public ItemRepository itemRepository() throws IOException {
-        InfluxDbConnectionManagerImpl connectionManager = new InfluxDbConnectionManagerImpl(
-                this.environment.getProperty("influx.url"),
-                this.environment.getProperty("influx.user"),
-                this.environment.getProperty("influx.pass")
-        );
-
-        String indexDir = this.environment.getProperty("index.dir.items");
-        LOGGER.info("Using {} as item index directory.", indexDir);
-
-        return new InfluxDbRepository(connectionManager, indexDir, true);
-    }
-
-    @Bean
-    public RecipeRepository recipeRepository() throws IOException {
-        String indexDir = this.environment.getProperty("index.dir.recipes");
-        LOGGER.info("Using {} as recipe index directory.", indexDir);
-
-        return new LuceneRecipeRepository(indexDir, true);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        if (environment.getProperty("resources.disableCaching", Boolean.class, false)) {
-            initDevelopmentResourceHandler(registry);
-        } else {
-            initProductionResourceHandler(registry);
-        }
-    }
-
-    private void initProductionResourceHandler(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-    }
-
-    private void initDevelopmentResourceHandler(ResourceHandlerRegistry registry) {
-        try {
-            String localFilesystem = new File("server/src/main/resources/static").toURI().toURL().toString();
-
-            registry.addResourceHandler("/static/**")
-                    .addResourceLocations(localFilesystem)
-                    .setCachePeriod(0)
-                    .resourceChain(false);
-        } catch (MalformedURLException e) {
-            LOGGER.error("Could not setup resource handler for local filesystem. Using the original one.", e);
-        }
-    }
-
-    @Bean
     public ViewResolver viewResolver() {
         EmbeddedVelocityViewResolver resolver = new EmbeddedVelocityViewResolver();
         this.properties.applyToViewResolver(resolver);
         return resolver;
-    }
-
-    @Bean
-    public FilterRegistrationBean localHostOnlyFilter() {
-        RemoteAddrFilter filter = new RemoteAddrFilter();
-        filter.setAllow("127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1");
-
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(filter);
-        registration.addUrlPatterns(
-                "/env",
-                "/metrics",
-                "/dump",
-                "/configprops",
-                "/mappings",
-                "/autoconfig",
-                "/health",
-                "/trace",
-                "/beans",
-                "/info",
-                "/admin/**"
-        );
-
-        return registration;
     }
 
     @Override
