1a078f54e0d6629d7c0b427c432079c37796b585
Stefan <slotties@gmail.com>
2016-02-08 19:17:03 +0100

    replaced velocity toolbox

--- server/src/main/java/gw2trades/server/VelocityRenderer.java
+++ server/src/main/java/gw2trades/server/VelocityRenderer.java
@@ -2,9 +2,12 @@ package gw2trades.server;
 
 import gw2trades.server.i18n.LocaleHandler;
 import gw2trades.server.i18n.Translator;
+import gw2trades.server.util.GuildWars2Util;
 import io.vertx.ext.web.RoutingContext;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.tools.generic.EscapeTool;
+import org.apache.velocity.tools.generic.MathTool;
 
 import java.io.StringWriter;
 import java.util.Map;
@@ -14,6 +17,10 @@ import java.util.ResourceBundle;
  * @author Stefan Lotties (slotties@gmail.com)
  */
 public class VelocityRenderer {
+    private static final GuildWars2Util GW2_UTIL = new GuildWars2Util();
+    private static final MathTool MATH_TOOL = new MathTool();
+    private static final EscapeTool ESCAPE_TOOL = new EscapeTool();
+
     private final VelocityEngine velocityEngine;
 
     public VelocityRenderer(VelocityEngine velocityEngine) {
@@ -29,6 +36,12 @@ public class VelocityRenderer {
         VelocityContext ctx = new VelocityContext(context);
         ctx.put("i18n", translator);
         ctx.put("currentLocale", LocaleHandler.getLocale(routingContext));
+
+        // Fill in some tools
+        ctx.put("mathtool", MATH_TOOL);
+        ctx.put("escapetool", ESCAPE_TOOL);
+        ctx.put("gw2", GW2_UTIL);
+
         velocityEngine.mergeTemplate(tpl, "UTF-8", ctx, output);
         output.flush();
 
